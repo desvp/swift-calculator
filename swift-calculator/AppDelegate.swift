713e3e30eed1d2ee5ee77dea7aecb9d9c3098ebc
Combo <combo@desvp.com>
2016-02-26 11:39:35 +0800

    remove storyboard

--- swift-calculator/AppDelegate.swift
+++ swift-calculator/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        let rootView: ViewController = ViewController()
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        if let window = self.window {
+            window.rootViewController = rootView
+            window.backgroundColor = UIColor.whiteColor()
+            window.makeKeyAndVisible()
+        }
+
         return true
     }
 
