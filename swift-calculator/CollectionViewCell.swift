96951f930fa082182f3d0b472681bdbf7b237bf6
Combo <combo@desvp.com>
2016-03-07 18:55:10 +0800

    update CollectionViewCell

--- swift-calculator/CollectionViewCell.swift
+++ swift-calculator/CollectionViewCell.swift
@@ -14,20 +14,23 @@ class CollectionViewCell: UICollectionViewCell {
         super.init(coder: aDecoder)
     }
 
-    var textLabel: UILabel!
-    var imageView: UIImageView!
+    var labelView: UILabel!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
-        imageView.contentMode = UIViewContentMode.ScaleToFill
-        contentView.addSubview(imageView)
-
         let textFrame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
-        textLabel = UILabel(frame: textFrame)
-        textLabel.font = UIFont.systemFontOfSize(CGFloat(60))
-        textLabel.textAlignment = .Center
-        contentView.addSubview(textLabel)
+        labelView = UILabel(frame: textFrame)
+        labelView.font = UIFont.systemFontOfSize(CGFloat(32), weight: UIFontWeightLight)
+        labelView.textAlignment = .Center
+        labelView.layer.borderColor = UIColor(RGB: 0x333333).CGColor
+        labelView.layer.borderWidth = 0.5
+
+//        labelView.layer.shadowColor = UIColor.darkGrayColor().CGColor
+//        labelView.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+//        labelView.layer.shadowOpacity = 0.8
+//        labelView.layer.shadowRadius = 2
+
+        contentView.addSubview(labelView)
     }
 }
